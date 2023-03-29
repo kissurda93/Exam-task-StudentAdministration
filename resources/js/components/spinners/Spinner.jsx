ab5678deb07c82a5b3f298ce69cef435c34b0464
kissurda93 <jg19930610@gmail.com>
2023-03-29 20:41:54 +0200

    Spinner component fixed

--- resources/js/components/spinners/Spinner.jsx
+++ resources/js/components/spinners/Spinner.jsx
@@ -3,7 +3,7 @@ import "./spinner.css";
 export default function Spinner({ small }) {
   return (
     <div className="spinner-container">
-      <div className={small ? "lds-ring small-spinner-container" : "lsd-ring"}>
+      <div className={small ? "lds-ring small-spinner-container" : "lds-ring"}>
         <div className={small ? "small-spinner" : ""}></div>
         <div className={small ? "small-spinner" : ""}></div>
         <div className={small ? "small-spinner" : ""}></div>
